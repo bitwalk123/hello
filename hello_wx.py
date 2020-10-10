a5addceaa21a04917413806ce3b5f778cc779a96
Keiichi Takahashi <bitwalk@gmail.com>
2020-10-10 19:56:47 +0900

    added wxPython sample for 'Hello World!' in Japanese.

---
+++ hello_wx.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import wx
+
+
+class Hello(wx.Frame):
+    def __init__(self, *args, **kw):
+        super(Hello, self).__init__(*args, **kw)
+        self.SetIcon(wx.Icon("./wx.ico"))
+
+        panel = wx.Panel(self)
+        stext = wx.StaticText(panel, label="こんにちは、世界！")
+        font = stext.GetFont()
+        font.PointSize = 24
+        stext.SetFont(font)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(stext, wx.SizerFlags().Border(wx.TOP | wx.LEFT, 25))
+        panel.SetSizer(sizer)
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    hello = Hello(None, title='wxPython edition')
+    hello.Show()
+    app.MainLoop()
