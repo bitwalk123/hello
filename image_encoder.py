c7d44000d2fb30c6758a8e9f0b08d92c067c0ef1
Fuhito Suguri <bitwalk@gmail.com>
2021-03-21 20:34:42 +0900

    update

--- image_encoder.py
+++ image_encoder.py
@@ -1,7 +1,9 @@
 from zlib import compress
 from base64 import b64encode
 
-with open("wx.png", "rb") as fileobj:
+#filename = 'wx.png'
+filename = 'qt.png'
+with open(filename, 'rb') as fileobj:
     #data = b64encode(compress(fileobj.read()))
     data = b64encode(fileobj.read())
 
